781968a65e3c2e05a0e4b26883ddfb0086a3b7f2
nhatsmrt <nphamcs@gmail.com>
2021-01-11 16:26:26 -0800

    Add dropout and batch norm to model

--- src/gnn.py
+++ src/gnn.py
@@ -10,16 +10,25 @@ class MultilayerGCN(nn.Module):
     def __init__(self, in_features: int, hidden_features: List[int], out_features: int, activation: Callable[[], nn.Module]=nn.ReLU):
         super().__init__()
 
-        layers = [GraphConv(in_feats=in_features, out_feats=hidden_features[0], activation=activation())]
+        layers = [GraphConv(in_feats=in_features, out_feats=hidden_features[0])]
+        dropouts = [nn.Dropout(0.5) for _ in range(len(hidden_features))]
+        batch_norms = [nn.BatchNorm1d(hidden_feature) for hidden_feature in hidden_features]
+        activations = [activation() for _ in range(len(hidden_features))]
 
         for i in range(1, len(hidden_features)):
             layers.append(GraphConv(hidden_features[i - 1], hidden_features[i], activation=activation()))
 
         layers.append(GraphConv(hidden_features[-1], out_features))
         self.layers = nn.ModuleList(layers)
+        self.dropouts = nn.ModuleList(dropouts)
+        self.batch_norms = nn.ModuleList(batch_norms)
+        self.activations = nn.ModuleList(activations)
 
     def forward(self, blocks, input_features):
         for i, layer in enumerate(self.layers):
-              input_features = layer(blocks[i], input_features)  # activation
+              input_features = layer(blocks[i], input_features)
+
+              if i < len(self.layers) - 1:
+                input_features = self.dropouts[i](self.activations[i](self.batch_norms[i](input_features)))
 
         return input_features
